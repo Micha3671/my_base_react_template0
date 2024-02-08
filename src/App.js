19904e3e62368b678a40086701f5c3e9a1dfa08a
Micha <michael.rigas@t-online.de>
2024-02-08 21:01:34 +0100

    Button mit Rückmeldung

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NavigationBar from "./components/layout/navbar";
 import "./styles/App.css";
 import "./styles/GlobalVars.css";
-import HomePage from "./pages/home-page";
+// import HomePage from "./pages/home-page";
+import InfoPage from "./pages/info-page";
 import ImpressumPage from "./pages/impressum-page";
 import Content from "./components/layout/content";
 import AboutMePage from "./pages/aboutme-page";
@@ -14,7 +15,8 @@ function App() {
       <NavigationBar />
       <Content>
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          {/* <Route path="/" element={<HomePage />} /> */}
+          <Route path="/" element={<InfoPage />} />
           <Route path="/impressum" element={<ImpressumPage />} />
           <Route path="/aboutme" element={<AboutMePage />} />
         </Routes>
